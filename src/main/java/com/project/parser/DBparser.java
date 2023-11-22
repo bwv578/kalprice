866ef52dba876eb6e5fbfc75038cc92571c7e9fe
bwv578 <nanopico912@gmail.com>
2023-11-23 06:53:13 +0900

    [fix] pdf파일 파서 - 가격 계산 시 무게단위 100그램 기준으로 통일

--- src/main/java/com/project/parser/DBparser.java
+++ src/main/java/com/project/parser/DBparser.java
@@ -29,17 +29,15 @@ public class DBparser {
 			if(fileSource.endsWith(".pdf")) {
 				PDDocument pdfDoc = PDDocument.load(source);
 				String text = new PDFTextStripper().getText(pdfDoc);
+				//System.out.println(text);
 				
 				// 본문을 페이지 단위로 분할 
 				String[] sheets = text.split("중분류 소분류 조사품목 원산지\\(제조사\\) 조사규격\\(품종\\) 조사단위 전주 금주 전주대비 전주 금주 전주대비 전주 금주 전주대비 전주 금주 전주대비 전주 금주 전주대비 전주 금주 전주대비");		
 				int totalPages = sheets.length;
 				
-				//System.out.println("총 페이지 수 - " + totalPages);
-				//System.out.println(text);
-				
 				// 2페이지부터 처리 (1페이지는 개황 및 전망자료)
 				for(int i=1; i<totalPages; i++) {
-					System.out.println("@@@@@@@" + i + "페이지@@@@@@@" );
+					System.out.println("@@@@@@@ " + i + " 페이지@@@@@@@" );
 					
 					// 페이지 내용을 행단위로 분할
 					String[] lines = sheets[i].split("\n");
@@ -47,66 +45,71 @@ public class DBparser {
 					// 각 행단위 처리
 					for(int j=0; j<lines.length; j++) {
 						String name; // 품목의 이름
-						int unit = 0; // 품목의 기준 단위(DB상 단위코드)
+						String unit = ""; // 품목의 기준 단위
 						String itemClass; // 품목의 분류
-						String priceAvg; // 전국 평균가격
-						String fluc; // 전국 평균가격의 전주대비 등락
-						String priceSeoul; // 서울가격
-						String priceBusan; // 부산가격
-						String priceDaegu; // 대구가격
-						String priceGwangju; // 광주가격
-						String priceDaejeon; // 대전가격
+						double priceAvg; // 전국 평균가격
+						double fluc; // 전국 평균가격의 전주대비 등락
+						double priceSeoul; // 서울가격
+						double priceBusan; // 부산가격
+						double priceDaegu; // 대구가격
+						double priceGwangju; // 광주가격
+						double priceDaejeon; // 대전가격
 						String separator = ""; // 행 데이터에서 품목 구성요소 영역과 가격영역 구분자
 						
-						// 품목 행의 조사단위 파악
-						//if(lines[j].contains(" 개 ")) unit = 1;
-						//if(lines[j].contains(" 마리 ")) unit = 2;
-						if(lines[j].contains(" 10g ")) unit = 3;
-						if(lines[j].contains(" 50g ")) unit = 4;
-						if(lines[j].contains(" 100g ")) unit = 5;
-						if(lines[j].contains(" 200g ")) unit = 6;
-						if(lines[j].contains(" 300g ")) unit = 7;
-						if(lines[j].contains(" 400g ")) unit = 8;
-						if(lines[j].contains(" 500g ")) unit = 9;
-						if(lines[j].contains(" kg ") || lines[j].contains(" 1kg ")) unit = 10;
-						if(lines[j].contains(" 360㎖ ")) unit = 11;
-						if(lines[j].contains(" 500㎖ ")) unit = 12;
-						if(lines[j].contains(" 950㎖ ")) unit = 13;
-						if(lines[j].contains(" 1000㎖ ") || lines[j].contains(" ℓ ") || lines[j].contains(" 1ℓ ")) unit = 14;
-						if(lines[j].contains(" 1.8ℓ ")) unit = 15;
+						// 행 데이터를 단어 단위로 분할, 단위정보 추출
+						String[] words = lines[j].split(" ");
+						for(int k=0; k<words.length; k++) {
+							boolean isUnit = 
+									words[k].contains("g") ||
+									words[k].contains("kg") ||
+									words[k].contains("㎖") ||
+									words[k].contains("ℓ");
+							
+							if(isUnit) unit = words[k];
+							if(unit.equals("kg")) unit = "1kg";
+							else if(unit.equals("7-8kg")) unit = "7kg";
+							else if(unit.contains("×")) unit = unit.substring(0, unit.indexOf("×"));
+							else if(unit.startsWith("(")) unit = unit.replaceAll("\\(", "");
+							else if(unit.endsWith(")")) unit = unit.substring(0, unit.indexOf("g") + 1);
+							if(unit.startsWith("살코기참치")) unit = unit.replaceAll("살코기참치", "");
+							if(unit.startsWith("치약")) unit = unit.replaceAll("치약", "");
+						}
 						
 						// 특정 품목의 유효한 물가정보를 담고있는 행인지 판별
 						boolean isValidData = false;
-						if(unit != 0) isValidData = true;
+						if(!unit.equals("")) isValidData = true;
 						
 						// 유효한 정보를 가진 행인 경우 -> 품목의 나머지 구성요소 특정
 						if(isValidData) {
 							// 품목정보와 가격영역 사이의 구분자 특정
 							if(lines[j].contains(" 100g ")) separator = " 100g ";
-							if(lines[j].contains(" 600g ")) separator = " 600g ";
-							if(lines[j].contains(" kg ")) separator = " kg ";
+							else if(lines[j].contains(" 600g ")) separator = " 600g ";
+							else if(lines[j].contains(" kg ")) separator = " kg ";
 							
-							if(lines[j].contains(" 포 ")) separator = " 포 ";
-							if(lines[j].contains(" 송이 ")) separator = " 송이 ";
-							if(lines[j].contains(" 통 ")) separator = " 통 ";
-							if(lines[j].contains(" 봉 ")) separator = " 봉 ";
-							if(lines[j].contains(" 포기 ")) separator = " 포기 ";
-							if(lines[j].contains(" 단 ")) separator = " 단 ";
-							if(lines[j].contains(" 마리 ")) separator = " 마리 ";
-							if(lines[j].contains(" 묶음 ")) separator = " 묶음 ";
-							if(lines[j].contains(" 상자 ")) separator = " 상자 ";
-							if(lines[j].contains(" 팩 ")) separator = " 팩 ";
-							if(lines[j].contains(" PET ")) separator = " PET ";
-							if(lines[j].contains(" 캔 ")) separator = " 캔 ";
-							if(lines[j].contains(" 병 ")) separator = " 병 ";
+							if(lines[j].contains(" 개 ")) separator = " 개 ";
+							else if(lines[j].contains(" 포 ")) separator = " 포 ";
+							else if(lines[j].contains(" 송이 ")) separator = " 송이 ";
+							else if(lines[j].contains(" 통 ")) separator = " 통 ";
+							else if(lines[j].contains(" 봉 ")) separator = " 봉 ";
+							else if(lines[j].contains(" 포기 ")) separator = " 포기 ";
+							else if(lines[j].contains(" 단 ")) separator = " 단 ";
+							else if(lines[j].contains(" 마리 ")) separator = " 마리 ";
+							else if(lines[j].contains(" 묶음 ")) separator = " 묶음 ";
+							else if(lines[j].contains(" 상자 ")) separator = " 상자 ";
+							else if(lines[j].contains(" 팩 ")) separator = " 팩 ";
+							else if(lines[j].contains(" PET ")) separator = " PET ";
+							else if(lines[j].contains(" 캔 ")) separator = " 캔 ";
+							else if(lines[j].contains(" 병 ")) separator = " 병 ";
 							
 							if(separator != "") {
 								// 구분자를 통해 행을 기본정보와 가격정보 영역으로 분할
 								String infos = lines[j].split(separator)[0];
+								if(lines[j].split(separator)[lines[j].split(separator).length - 1].contains(" - ")) continue;
 								String[] prices = lines[j].split(separator)[lines[j].split(separator).length - 1].split(" ");
 								
 								// 기본정보로부터 분류, 이름 특정 
 								itemClass = infos.split(" ")[0];
+								if(itemClass.equals("세제류") || itemClass.equals("위생용품") || itemClass.equals("연료류") || itemClass.equals("귀금속류")) continue;
 								name = infos.split(" ")[1];
 								
 								// 디버깅용
@@ -119,13 +122,18 @@ public class DBparser {
 								}
 								*/
 								
-								priceAvg = prices[1];
-								fluc = prices[2];
-								priceSeoul = prices[4];
-								priceBusan = prices[7];
-								priceDaegu = prices[10];
-								priceGwangju = prices[13];
-								priceDaejeon = prices[16];
+								double devider = 1;
+								if(unit.contains("k")) devider = Double.parseDouble(unit.substring(0, unit.indexOf("k"))) * 10;
+								else if(unit.contains("g")) devider = Double.parseDouble(unit.substring(0, unit.indexOf("g"))) / 100;
+								//System.out.println(devider);
+								
+								priceAvg = Double.parseDouble(prices[1].replaceAll(",", "")) / devider;
+								fluc = Double.parseDouble(prices[2].replaceAll(",", ""));
+								priceSeoul = Double.parseDouble(prices[4].replaceAll(",", "")) / devider;
+								priceBusan = Double.parseDouble(prices[7].replaceAll(",", "")) / devider;
+								priceDaegu = Double.parseDouble(prices[10].replaceAll(",", "")) / devider;
+								priceGwangju = Double.parseDouble(prices[13].replaceAll(",", "")) / devider;
+								priceDaejeon = Double.parseDouble(prices[16].replaceAll(",", "")) / devider;
 								
 								// 처리결과 출력
 								System.out.println(
@@ -150,10 +158,7 @@ public class DBparser {
 			if(fileSource.endsWith(".xls") || fileSource.endsWith(".xlsx")){
 				FileInputStream fis = new FileInputStream(fileSource);
 				Workbook workbook = null;
-				
-				// 출력결과 테스트
-                StringBuilder result = new StringBuilder();
-				
+
 				// 엑셀 확장자에 따른 워크북 객체 생성
 				if(fileSource.endsWith(".xls")) {
 					// xls
@@ -259,7 +264,7 @@ public class DBparser {
 	    	                		else if(words[i].equals("500㎖")) unit = 12;
 	    	                		else if(words[i].equals("950㎖")) unit = 13;
 	    	                		else if(words[i].equals("1ℓ")) unit = 14;
-	    	                		else if(words[i].equals("1.8ℓ")) unit = 14;
+	    	                		else if(words[i].equals("1.8ℓ")) unit = 15;
 	                			}
 	                		}
 	                	}	
