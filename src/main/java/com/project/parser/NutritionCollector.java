104e122d3fdb88f41b99b466f753d6dbe92783a4
bwv578 <nanopico912@gmail.com>
2023-11-26 06:54:59 +0900

    [add] String -> xml 파싱코드 추가

--- src/main/java/com/project/parser/NutritionCollector.java
+++ src/main/java/com/project/parser/NutritionCollector.java
@@ -3,11 +3,21 @@ package com.project.parser;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 @Component
 public class NutritionCollector {
@@ -16,12 +26,12 @@ public class NutritionCollector {
 	@Value("${foodsafetykorea.api.key}")
 	private String apikey;
 	
-	// api 요청주소
-	String apiSource = "http://openapi.foodsafetykorea.go.kr/api/" + apikey + "/I0960/xml/1/5";
-	
 	public void getInfo() {
 		try {
-			System.out.println(apikey);
+			// api 요청주소
+			String apiSource = "http://openapi.foodsafetykorea.go.kr/api/" 
+			+ apikey 
+			+ "/I2790/xml/1/100/DESC_KOR=백미";
 			
 			URL url = new URL(apiSource);
 			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
@@ -37,7 +47,28 @@ public class NutritionCollector {
             }
             reader.close();
 			
-            System.out.println(response);
+            // xml로 파싱
+            try {
+            	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder builder = factory.newDocumentBuilder();
+				Document doc = builder.parse(new InputSource(new StringReader(response.toString())));
+				
+				NodeList rows = doc.getElementsByTagName("row");
+				
+				for(int i=0; i<rows.getLength(); i++) {
+					Element row = (Element)rows.item(i);
+					
+					String id = row.getAttribute("id");
+					String name = row.getElementsByTagName("DESC_KOR").item(0).getTextContent();
+					System.out.println(id + "  " + name);
+				}
+				
+			} catch (Exception e) {
+				System.out.println("xml 파싱 오류 - " + e.getMessage());
+			}
+
+            // 응답 출력
+            //System.out.println(response);
             conn.disconnect();
             
 		}catch(IOException e) {
